33e84de4f4a6dc4be93d9ba0bb5e5c60c3bd153b
Santiago Sanchis <gato.jijen01@gmail.com>
2026-04-09 18:24:58 -0500

    test

--- src/components/Downbar.tsx
+++ src/components/Downbar.tsx
@@ -10,12 +10,12 @@ interface DownbarProps {
 
 const Downbar = ({showHeart, showUsers, showWindow}: DownbarProps) => {
     return (
-        <nav className=' left-[50%] -translate-x-[50%] z-[999]
+        <nav className=' left-[50%] -translate-x-[50%] z-[999] hidden
                  w-[90%] max-w-md h-14 absolute
                  bottom-4
                 bg-white/80 backdrop-blur-xl border border-white/50 shadow-xl 
                 shadow-[#E197AC]
-                rounded-3xl px-8 py-4 flex items-center gap-8 justify-center
+                rounded-3xl px-8 py-4  items-center gap-8 justify-center
                 transition-all duration-300'>
             <Link href={'/'} className={`p-2 rounded-full  backdrop-blur-xl flex w-12 h-12 justify-center items-center hover:scale-105 cursor-pointer transition-all ${showWindow? 'bg-[#E197AC]': 'bg-transparent  text-[#E197AC] fill-[#E197AC]'}`}>
                 <span>
